9fb7aeeb8f596a70553f57a4595339e61944aa8e
Simon Mölzer <evilhowser@gmail.com>
2017-12-01 11:58:37 +0100

    Add client connection and read messages

--- src/client.py
+++ src/client.py
@@ -6,6 +6,12 @@
 
 import socket
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "epsilon.systems"
+channel = "#radio"
+port = 6667
+nick = "listener"
+
 def joinchan(chan):
     sock.send(bytes("JOIN " + chan + "\n", "UTF-8"))
     ircmsg = ""
@@ -23,22 +29,38 @@ def ircquit(msg):
 def playsong(song):
     pass
 
+def connect(server, port):
+    sock.connect((server, port))
+    sock.send(bytes("USER " + nick + " " + nick + " " + nick + " " + nick + "\n", "UTF-8"))
+    sock.send(bytes("NICK " + nick + "\n", "UTF-8"))
+
+def parseuri(msg):
+    index = msg.rfind("SONG:")
+    if (index == -1):
+        return ""
+    else:
+        return msg.sub(index)
+
+def exit():
+    sock.close()
+
+def pingpong(server):
+    None
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server = "epsilon.systems"
-channel = "#radio"
-port = 6667
-nick = "listener"
-
-sock.connect((server, port))
-sock.send(bytes("USER " + nick + " " + nick + " " + nick + " " + nick + "\n", "UTF-8"))
-sock.send(bytes("NICK " + nick + "\n", "UTF-8"))
-
+connect(server, port)
 joinchan(channel)
 sendmsg("im here", channel)
 
 while True:
     msg = sock.recv(2048).decode("UTF-8")
     msg = msg.strip('\n\r')
+
+    if msg == "": # quit if we don't receive data
+        exit()
+        break
+
     print(msg)
+
+    if (msg.find("PRIVMSG ") > -1):
+        print("found msg")
